a46248939f2a1ae3aa782c1f03e1130a3a59410e
scalethread <scalethread@gmail.com>
2026-08-26 15:06:56 +0530

    chore: configure redux store

---
+++ src/store/hook.ts
@@ -0,0 +1,7 @@
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from './index';
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: <TSelected>(selector: (state: RootState) => TSelected) => TSelected =
+  useSelector;
